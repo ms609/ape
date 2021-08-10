e6c5289d24006349df77c62c5137b115fc3ee2bf
Martin R. Smith <martin.smith@durham.ac.uk>
2021-08-10 08:26:11 +0100

    Tally as we go
    Need to validate calculation of _n_ in `qbinom()`

--- tests/testthat/test-RCOAL.R
+++ tests/testthat/test-RCOAL.R
@@ -4,21 +4,17 @@ test_that("Random coalescent trees", {
   BOUND <- qnorm(bound_p)
   N <- 100
   tree.sizes <- c(5, 10, 20, 50, 75, 100)
-  falsePositiveRate <- 0.01
-  res <- numeric()
-  for (i in 1:200) {
+  failures <- 0L
+  reps <- 200L
+  for (i in seq_len(reps)) {
     for (n in tree.sizes) {
       k <- 2:n
       expected.mean <- 2 * sum(1/(k * (k - 1)))
       expected.var <- 4 * sum(1/(k * (k - 1))^2)
       x <- replicate(N, branching.times(rcoal(n))[1])
-      res <- c(res, (mean(x) - expected.mean) * sqrt(N/expected.var))
-    }
-    if (anyNA(res)) {
-      message("Missing values returned after", length(res), "simulations")
-      expect_true(FALSE)
+      expect_false(anyNA(x))
+      failures <- failures + sum(abs((mean(x) - expected.mean) * sqrt(N/expected.var)) > BOUND)
     }
   }
-  tab <- tabulate((abs(res) > BOUND) + 1L, 2L)[2]
-  expect_lte(pbinom(tab, length(res), 1 - bound_p), 1 - falsePositiveRate)
+  expect_lte(pbinom(failures, reps * length(tree.sizes) * N, 1 - bound_p), bound_p)
 })
